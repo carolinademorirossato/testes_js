7ecbf21a6d0f6393664761b3788a3f0b6b6455cb
Carolina Demori Rossato <crossato2147@gmail.com>
2025-02-05 13:37:45 -0300

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,4 @@
+let x = 38;
+let y = 9;
+let z = x % y;
+document.getElementById("demo").innerHTML = "O resto de x % y é: " + z;
---
+++ variaveis.js
@@ -0,0 +1,18 @@
+let curso = "Curso de JavaScript";
+alert(curso);
+
+let mensagem = "Olá mundo";
+console.log(mensagem);
+
+let cursos = "tecnologia";
+cursos.length;
+cursos.replace("tecno","socio");
+document.getElementById("cursos").innerHTML = "O novo curso é " + curso.replace;
+
+let idade = 10;
+let age = 30;
+idade += 10;
+idade -= 5;
+idade /= 3;
+age *= 10; 
+document.getElementById("idade").innerHTML = "A idade calculada é " + idade;
